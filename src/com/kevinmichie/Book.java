aa420b36bd2a968e91103072bbda0853cc1e1962
Kevin Michie <kmichie32@gmail.com>
2016-04-18 19:16:50 -0500

    Practice with Inheritance and Casting

---
+++ src/com/kevinmichie/Book.java
@@ -0,0 +1,15 @@
+package com.kevinmichie;
+
+public abstract class Book {
+
+    String title;
+    String author;
+    Book(String t,String a){
+        title=t;
+        author=a;
+    }
+
+
+    abstract void display();
+
+}
